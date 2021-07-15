ab292e8e66db58b1822d3158e3a2aee74fb8a1ee
MahatmaK <nirajkhatri21@gmail.com>
2021-07-15 17:49:38 -0400

    Added legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,15 +1,10 @@
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-
-
-  var earthquakes = data.features;
-  var numEarthquake = earthquakes.length;
-
+  // Define map
   var myMap = L.map("map", {
       center: [34.0522, -118.2437],
       zoom: 4
     });
 
-
+  // Add light map tile
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -20,18 +15,33 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     accessToken: API_KEY
   }).addTo(myMap);
 
+var geojson;
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+
+  var earthquakes = data.features;
+  var numEarthquake = earthquakes.length;
+  
+  
+
+  // geojson = L.choropleth(data, {
+
+  //   valueProperty: geometry.coordinates[2],
+  //   scale: ["rgb(163, 246, 0)", "rgb(163, 246, 0)"],
+  //   steps: 6,
+  //   mode: "q",
+  //   style: {
+  //     color: "black",
+  //     weight:1, 
+  //     fillOpacity:1,
+  //   },
+
+  //   onEachFeature: function(fea
 
 
-
-
-
-  // console.log(earthquakes[0].geometry.coordinates);
-  // console.log(earthquakes[0].geometry.coordinates[0]);
-  // console.log(earthquakes[0].properties.place);
+  // })
   
   for (let i = 0; i < numEarthquake ; i++) {
-
-    console.log(earthquakes[i].geometry.coordinates[2]);
 
     var color = "";
     if  (earthquakes[i].geometry.coordinates[2] < 10) {
@@ -56,9 +66,41 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       fillColor: color,
       fillOpacity: 1,
       radius: earthquakes[i].properties.mag * 2
-    }).bindPopup("<h2>" + earthquakes[i].properties.place + "</h2> <hr>" + "<h3> Magnitude: " + earthquakes[i].properties.mag + "</h3>")
+    }).bindPopup("<h2>" + earthquakes[i].properties.place + "</h2> <hr>" + "<h3> Magnitude: " + earthquakes[i].properties.mag + "</h3>" + "<h3> Depth: " + earthquakes[i].geometry.coordinates[2] + "</h3>")
     .addTo(myMap);
   }
 
 
+  // LEGEND //
+  
+  // Define location of legend
+  var legend = L.control({position: 'bottomright'});
+
+  // Define getColor function
+  function getColor(d) {
+    return d > 90 ? "rgb(255, 95, 101)" : 
+    d > 70 ? "rgb(252, 163, 93)" :
+    d > 50 ? "rgb(253, 183, 42)" : 
+    d > 30 ? "rgb(247, 219, 17)" :
+    d > 10 ? "rgb(220, 244, 0)" :
+             "rgb(163, 246, 0)";
+  }
+
+  legend.onAdd = function () {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+    grades = [-10, 10, 30, 50, 70, 90],
+    labels = [];
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+          '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
+    return div;
+  };
+
+  legend.addTo(myMap);
+
 });
+    
